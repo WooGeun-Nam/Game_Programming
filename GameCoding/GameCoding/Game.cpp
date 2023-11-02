5ef862f50fa19eaeb20b81a94dbdf9da539084cf
WooGeun-Nam <rolru7686@gmail.com>
2023-11-02 13:24:15 +0900

    Resource Manager

--- GameCoding/GameCoding/Game.cpp
+++ GameCoding/GameCoding/Game.cpp
@@ -3,6 +3,7 @@
 #include "TimeManager.h"
 #include "InputManager.h"
 #include "SceneManager.h"
+#include "ResourceManager.h"
 
 Game::Game()
 {
@@ -11,6 +12,7 @@ Game::Game()
 Game::~Game()
 {
 	GET_SINGLE(SceneManager)->Clear();
+	GET_SINGLE(ResourceManager)->Clear();
 
 	_CrtDumpMemoryLeaks();
 }
@@ -30,6 +32,7 @@ void Game::init(HWND hwnd)
 	GET_SINGLE(TimeManager)->Init();
 	GET_SINGLE(InputManager)->Init(hwnd);
 	GET_SINGLE(SceneManager)->Init();
+	GET_SINGLE(ResourceManager)->Init();
 
 	GET_SINGLE(SceneManager)->ChangeScene(SceneType::GameScene);
 }
